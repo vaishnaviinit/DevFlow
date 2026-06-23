3c538a40ebc80111ecbdc845df922c292cc3dbc1
vaishnaviinit <vaishnavipixy@gmail.com>
2026-06-23 21:50:17 +0530

    add auth schema fields and prisma config

---
+++ backend/src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || "5000",
+  JWT_SECRET: process.env.JWT_SECRET || "",
+  DATABASE_URL: process.env.DATABASE_URL || "",
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-
+import authRoutes from "./modules/auth/auth.routes";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("DevFlow Backend Running");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
